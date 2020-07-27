44d4dcdc6bfef8e9b1e7d856a44dd4dd2e79dcdc
James Collinson <j.collinson@skatelescope.org>
2020-07-27 18:40:38 +0100

    ESCAP-154 Refactoring Image2d class

--- ska/sdc1/models/image_2d.py
+++ ska/sdc1/models/image_2d.py
@@ -3,80 +3,158 @@ import os
 import montage_wrapper as montage
 import numpy as np
 from astropy import units as u
-from astropy.coordinates import SkyCoord
 from astropy.io import fits
-from astropy.nddata.utils import Cutout2D
-from astropy.wcs import WCS
-from astropy.wcs.utils import pixel_to_skycoord, skycoord_to_pixel
 
+from ska.sdc1.models.exceptions import ImageNotPreprocessed
 from ska.sdc1.utils.image_utils import (
     crop_to_training_area,
     get_image_centre_coord,
     get_pixel_value_at_skycoord,
     save_subimage,
-    update_header_from_cutout2D,
 )
 
 
 class Image2d:
-    def __init__(self, freq, path, pb_path, prep=False):
+    """
+    Class to handle image preprocessing for SDC1.
+
+    The Image2d instance contains simple metadata about the SDC1 image, such as
+    frequency and the paths to the files on disk, and provides methods to perform
+    preprocessing of the images.
+
+    Args:
+        freq (:obj:`int`): Image frequency band (560, 1400 or 9200 MHz)
+        path (:obj:`str`): Path to SDC1 raw image
+        pb_path (:obj:`str`): Path to corresponding PrimaryBeam image
+    """
+
+    def __init__(self, freq, path, pb_path):
         self._freq = freq
         self._path = path
         self._pb_path = pb_path
-        self._prep = prep
+        self._prep = False
 
         self._train = None
+        self._pb_corr_image = None
 
     @property
     def freq(self):
+        """
+        Image frequency band (560, 1400 or 9200 MHz)
+        """
         return self._freq
 
     @property
     def path(self):
+        """
+        Path to SDC1 raw image
+        """
         return self._path
 
     @property
+    def basepath(self):
+        """
+        Path of directory containing image path
+        """
+        return os.path.dirname(self._path)
+
+    @property
     def pb_path(self):
+        """
+        Path to corresponding PrimaryBeam image
+        """
         return self._pb_path
 
     @property
+    def prep(self):
+        """
+        Boolean which states whether image preprocessing has been performed
+        """
+        return self._prep
+
+    @property
     def train(self):
-        return self._train
+        """
+        Path to training image (if self.prep=True)
+        """
+        if self.prep:
+            return self._train
+        else:
+            raise ImageNotPreprocessed("No training image, image not preprocessed")
 
     @property
-    def pb_cut_path(self):
-        return self._pb_path[:-5] + "_cut.fits"
+    def pb_corr_image(self):
+        """
+        Path to primary beam corrected image (if self.prep=True)
+        """
+        if self.prep:
+            return self._pb_corr_image
+        else:
+            raise ImageNotPreprocessed("No PB-corrected image, image not preprocessed")
 
-    @property
-    def pb_cut_rg_path(self):
-        return self._pb_path[:-5] + "_cut_rg.fits"
-
-    def preprocess(self, overwrite=True):
+    def preprocess(self):
         """
         Perform preprocessing steps:
-        1) Apply PB correction
-        2) Output separate training image
-        3) Only output necessary data dimensions
+            1) Create PB-corrected image (self.pb_corr_image)
+            2) Output separate training image (self.train)
         """
-        self._apply_pb_corr()
-        self._create_train(overwrite)
+        self._create_pb_corr()
+        self._create_train()
+
+    def reset(self):
+        """
+        Reset the state of the Image2d instance by deleting train and PB-corrected
+        images
+        """
+        self._delete_pb_corr()
+        self._delete_train()
+
+    def _get_pb_cut_path(self):
+        """
+        PB cutout path (temporary)
+        """
+        return self._pb_path[:-5] + "_cut.fits"
+
+    def _get_pb_cut_rg_path(self):
+        """
+        PB cutout regridded path (temporary)
+        """
+        return self._pb_path[:-5] + "_cut_rg.fits"
+
+    def _get_hdr_path(self):
+        """
+        Montage header file, used for reproject (temporary)
+        """
+        return os.path.join(self.basepath, "hdu_tmp.hdr")
 
     def _create_train(self, pad_factor=1.0):
+        """
+        Create the training image (crop to the frequency-dependent training area)
+        """
         self._train = None
         train_path = self.path[:-5] + "_train.fits"
-        crop_to_training_area(self.path, train_path, self.freq, pad_factor)
+        crop_to_training_area(self._pb_corr_image, train_path, self.freq, pad_factor)
         self._train = train_path
 
-    def _apply_pb_corr(self):
+    def _delete_train(self):
         """
-        How to perform PB correction depends on the ratio of image size to
-        PB image pixel size; if image size is comparable or smaller than
-        a single PB pixel, the reprojection with Montage will fail, but the
-        correction can be approximated by taking the PB value at the input
-        image centre.
-        If the image is only a little larger than this, there can be a lack
-        of overlap with the PB image when reprojecting, so increase the size
+        Delete the training image
         """
+        if self._train is None:
+            return
+        if os.path.exists(self._train):
+            os.remove(self._train)
+        self._train = None
+
+    def _create_pb_corr(self):
+        """
+        Apply PB correction to the image at self.path, using the primary beam
+        file at self.pb_path.
+
+        This uses Montage to regrid the primary beam image to the same pixel scale
+        as the image to be corrected.
+        """
+        self._pb_corr_image = None
 
         # Establish input image to PB image pixel size ratios:
         with fits.open(self.pb_path) as pb_hdu:
@@ -86,81 +164,102 @@ class Image2d:
             x_pixel_deg = image_hdu[0].header["CDELT2"]
 
         ratio_image_pb_pix = (x_size * x_pixel_deg) / pb_x_pixel_deg
+        coord_image_centre = get_image_centre_coord(self.path)
 
         if ratio_image_pb_pix < 2.0:
             # Image not large enough to regrid (< 2 pixels in PB image);
             # apply simple correction
-            coord_image_centre = get_image_centre_coord(self.path)
             pb_value = get_pixel_value_at_skycoord(self.pb_path, coord_image_centre)
-            self._write_pb_corr(pb_value)
+            self._apply_pb_corr(pb_value)
             return
 
-        if ratio_image_pb_pix < 4.0:
-            # Montage complains of a lack of overlap if image is small
-            pad_cutout = 5.0
-        else:
-            pad_cutout = 1.0
-
         with fits.open(self.pb_path) as pb_hdu:
-            # RA and DEC of beam PB pointing
-            pb_pos = SkyCoord(
-                pb_hdu[0].header["CRVAL1"] * u.degree,
-                pb_hdu[0].header["CRVAL2"] * u.degree,
-            )
-
+            # Create cropped PB image of the same size as the input image
             size = (
-                x_size * x_pixel_deg * u.degree * pad_cutout,
-                x_size * x_pixel_deg * u.degree * pad_cutout,
+                x_size * x_pixel_deg * u.degree,
+                x_size * x_pixel_deg * u.degree,
             )
 
-            save_subimage(self.pb_path, self.pb_cut_path, pb_pos, size, overwrite=True)
+            save_subimage(
+                self.pb_path,
+                self._get_pb_cut_path(),
+                coord_image_centre,
+                size,
+                overwrite=True,
+            )
 
-        # TODO: Regrid PB image cutout to match pixel scale of the image FOV
-        print(" Regridding image...")
-        # get header of image to match PB to
-        montage.mGetHdr(self.path, "hdu_tmp.hdr")
-        # regrid pb image (270 pixels) to size of ref image (32k pixels)
+        # Regrid image PB cutout to same pixel scale as input image
+        montage.mGetHdr(self.path, self._get_hdr_path())
         montage.reproject(
-            in_images=self.pb_cut_path,
-            out_images=self.pb_cut_rg_path,
-            header="hdu_tmp.hdr",
+            in_images=self._get_pb_cut_path(),
+            out_images=self._get_pb_cut_rg_path(),
+            header=self._get_hdr_path(),
             exact_size=True,
         )
-        os.remove("hdu_tmp.hdr")  # get rid of header text file saved to disk
+        os.remove(self._get_hdr_path())
 
-        # do pb correction
-        with fits.open(self.pb_cut_rg_path, mode="update") as pb_hdu:
+        # Correct Montage output (convert to 32-bit and fill NaNs)
+        pb_array = self._postprocess_montage_out()
+
+        # Apply PB correction and delete temporary files
+        self._apply_pb_corr(pb_array)
+        self._cleanup_pb()
+
+    def _delete_pb_corr(self):
+        """
+        Delete the PB-corrected image
+        """
+        if self._pb_corr_image is None:
+            return
+        if os.path.exists(self._pb_corr_image):
+            os.remove(self._pb_corr_image)
+        self._pb_corr_image = None
+
+    def _postprocess_montage_out(self):
+        """
+        Montage outputs the regridded PB image using 64-bit floats, and pads edges
+        with NaN values - fix these issues.
+        """
+        with fits.open(self._get_pb_cut_rg_path(), mode="update") as pb_hdu:
             newdata = np.zeros(
                 (1, 1, pb_hdu[0].data.shape[0], pb_hdu[0].data.shape[1]),
                 dtype=np.float32,
             )
             newdata[0, 0, :, :] = pb_hdu[0].data
-            # naxis will automatically update to 4 in the header
+            # NAXIS will automatically update to 4 in the header
             pb_hdu[0].data = newdata
 
-            # fix nans introduced in primary beam by montage at edges
+            # Fix NaNs introduced in PB by Montage at edges
+            # TODO: This may not be performing correctly, check
             mask = np.isnan(pb_hdu[0].data)
             pb_hdu[0].data[mask] = np.interp(
                 np.flatnonzero(mask), np.flatnonzero(~mask), pb_hdu[0].data[~mask]
             )
             pb_array = pb_hdu[0].data
             pb_hdu.flush()
+        return pb_array
 
-        self._write_pb_corr(pb_array)
-
-    def _write_pb_corr(self, pb_data):
+    def _apply_pb_corr(self, pb_data):
         """
         Apply the PB correction and write to disk.
-        
+
         pb_data can either be an array of the same dimensions as the image at
         self.path, or a scalar.
         """
-        pb_corr_path = self.path[:-5] + "_pb_corr.fits"
+        pb_corr_path = self.path[:-5] + "_pbcor.fits"
         with fits.open(self.path) as image_hdu:
             image_hdu[0].data = image_hdu[0].data / pb_data
             image_hdu[0].writeto(pb_corr_path, overwrite=True)
+        self._pb_corr_image = pb_corr_path
 
-    def _delete_train(self):
-        if self.train is None:
-            return
-        os.remove(self._train)
+    def _cleanup_pb(self):
+        """
+        Remove temporary files created to apply PB correction
+        """
+        for tempfile in [
+            self._get_pb_cut_path(),
+            self._get_pb_cut_rg_path(),
+            self._get_hdr_path(),
+        ]:
+            if os.path.exists(tempfile):
+                os.remove(tempfile)
--- tests/unittests/sdc1/test_image_2d.py
+++ tests/unittests/sdc1/test_image_2d.py
@@ -6,19 +6,24 @@ from ska.sdc1.models.image_2d import Image2d
 class TestImage2d:
     def test_preprocess(self, images_dir, test_image_name, pb_image_name):
         train_file_expected = test_image_name[:-5] + "_train.fits"
+        pbcor_file_expected = test_image_name[:-5] + "_pbcor.fits"
         test_image_path = os.path.join(images_dir, test_image_name)
         pb_image_path = os.path.join(images_dir, pb_image_name)
 
         # Before running preprocess, the segment and train files shouldn't exist
-        assert os.path.isfile(os.path.join(images_dir, train_file_expected)) is False
-        image_2d = Image2d(560, test_image_path, pb_image_path, prep=False)
-        image_2d.preprocess(overwrite=True)
+        for expected_file in [train_file_expected, pbcor_file_expected]:
+            assert os.path.isfile(os.path.join(images_dir, expected_file)) is False
+        image_2d = Image2d(560, test_image_path, pb_image_path)
+        image_2d.preprocess()
 
         # Check files have been created
-        assert os.path.isfile(os.path.join(images_dir, train_file_expected))
+        for expected_file in [train_file_expected, pbcor_file_expected]:
+            assert os.path.isfile(os.path.join(images_dir, expected_file))
 
         # Delete them again
         image_2d._delete_train()
+        image_2d._delete_pb_corr()
 
         # Verify
-        assert os.path.isfile(os.path.join(images_dir, train_file_expected)) is False
+        for expected_file in [train_file_expected, pbcor_file_expected]:
+            assert os.path.isfile(os.path.join(images_dir, expected_file)) is False
