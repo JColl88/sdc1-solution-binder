938238f8d8b8a7bbc7cc75e88ca2dddd9d31cd95
rob <r.barnsley@skatelescope.org>
2020-07-29 15:18:46 +0100

    Added SKLRegression class.

--- ska/sdc1/utils/classification.py
+++ ska/sdc1/utils/classification.py
@@ -9,57 +9,77 @@ from ska.sdc1.utils.bdsf_utils import cat_df_from_srl, load_truth_df, srl_gaul_d
 from ska.sdc1.utils.columns from utils import SRL_CAT_COLS, SRL_COLS_TO_DROP, SRL_NUM_COLS
 
 
-'''def get_xmatch_df(cat_df, truth_df, freq):
-    """
-    Get the matched sources DataFrame for the passed catalogue DataFrame. Uses the
-    scorer to yield the match catalogue.
-
-    Args:
-        cat_df (:obj:`pandas.DataFrame`): Catalogue DataFrame to obtain match_df for
-        truth_df (:obj:`pandas.DataFrame`): Truth catalogue DataFrame
-        freq: (`int`): Frequency band
-    """
-    scorer = Sdc1Scorer(cat_df, truth_df, freq)
-    score = scorer.run(train=True, detail=True, mode=1)
-
-    return score.match_df'''
-
-
-def SKLRegression(Model):
-    """ 
-        Base class for regression using scikit-learn. 
-    """
-    def __init__(self, srl_path, truth_cat_path, gaul_path, freq=1400):
+class SKLRegression():
+    def __init__(self, algorithm=RandomForestRegressor, regressor_args=[], 
+    regressor_kwargs={}):
         """
+        Sci-kit learn regression.
+        
         Args:
-            srl_path (`str`): Path to source list (.srl file).
-            truth_cat_path (`str`): Path to truth catalogue.
-            gaul_path (`str`): Path to gaussian list (.srl file).
-            freq: (`int`): Frequency band (MHz).
+            algorithm (`class`): SKL regressor class.
+            regressor_args (`list`): regressor args.
+            regressor_kwargs (`dict`): regressor kwargs.
         """
-        self.srl_path = srl_path
-        self.truth_cat_path = truth_cat_path
-        self.gaul_path = gaul_path
-        self.freq = freq
-        self.regressor = None
+        self.regressor = algorithm(*regressor_args, **regressor_kwargs)
 
 
-    def _fit(self, X, y):
+    def _preprocess_srl_df(self, srl_df, srl_cat_cols, srl_num_cols, srl_drop_cols):
         """
-        Fits training input samples, X, against target values, y.
+        Preprocess the source list DataFrame ready for model generation and 
+        prediction.
+
+        Args:
+            srl_df (:obj:`pandas.DataFrame`): Source list.
+            srl_cat_cols: (`list`) Categorical columns in source list.
+            srl_num_cols: (`list`) Numerical columns in source list.
+            srl_drop_cols: (`list`) Columns to exclude in source list.
+        Returns:
+            (:obj:`pandas.DataFrame`): Processed source list.
+        """
+        # Drop NaNs.
+        #
+        srl_df = srl_df.dropna()
+
+        # Drop obsolete columns.
+        #
+        srl_df = srl_df.drop(srl_drop_cols, axis=1)
+
+        # Encode categorical columns.
+        #
+        for col in srl_cat_cols:
+            lbl = LabelEncoder()
+            lbl.fit(list(srl_df[col].values.astype("str")))
+            srl_df[col] = lbl.transform(list(srl_df[col].values.astype("str")))
+
+        # Cast numerical columns as floats.
+        #
+        for col in srl_num_cols:
+            srl_df[col] = srl_df[col].astype(float)
+
+        return srl_df
+
+
+    def _SKLfit(self, X, y):
+        """
+        Wrapper for SKL fit(). 
+        
+        Fits training input samples, X[X_columns], against target values, 
+        y[Y_column].
 
         Args:
             X (:obj:`pandas.DataFrame`): Training input samples.
             y (:obj:`numpy.array`): Target values.
         Returns:
             None
-        """
+        """              
         self.regressor.fit(train_x, train_y)
 
 
-    def _predict(self, X):
+    def _SKLpredict(self, X):
         """
-        Predicts values, y, from input samples X.
+        Wrapper for SKL predict().
+
+        Predicts values, y, from input samples X[X_columns].
 
         Args:
             X (:obj:`pandas.DataFrame`): Input samples.
@@ -69,126 +89,96 @@ def SKLRegression(Model):
         return regressor.predict(test_x)
 
 
-    def _preprocess_srl_df(self, srl_df):
+    def train(self, srl_path, truth_cat_path, gaul_path, regressand_col, freq=1400, 
+    srl_cat_cols=SRL_CAT_COLS, srl_num_cols=SRL_NUM_COLS,
+    srl_drop_cols=SRL_COLS_TO_DROP):
         """
-        Preprocess the source list DataFrame for model generation and prediction.
+        Train the regressor on <regressand_col> using a crossmatched PyBDSF 
+        source list.
 
-        Args:
-            srl_df (:obj:`pandas.DataFrame`): Source list.
-        Returns:
-            (:obj:`pandas.DataFrame`): Processed source list.
-        """
-
-        # Drop NaNs.
-        #
-        srl_df = srl_df.dropna()
-
-        # Drop obsolete columns.
-        #
-        srl_df = srl_df.drop(SRL_COLS_TO_DROP, axis=1)
-
-        # Encode categorical columns.
-        #
-        for col in SRL_CAT_COLS:
-            lbl = LabelEncoder()
-            lbl.fit(list(srl_df[col].values.astype("str")))
-            srl_df[col] = lbl.transform(list(srl_df[col].values.astype("str")))
-
-        # Cast numerical columns as floats.
-        #
-        for col in SRL_NUM_COLS:
-            srl_df[col] = srl_df[col].astype(float)
-
-        return srl_df
-
-
-    def run(column):
-        """
-        Given a PyBDSF source list, create an SDC1 catalogue and obtain the
-        match catalogue from the score pipeline, then use the truth catalogue's size
-        values together with the source list properties to build a predictive model.
-
-        Args:
-            column (`str`): b_maj_t or b_min_t
-        """
-
-        # Crossmatch the dataframe.
-        #
-        sub_cat_df = cat_df_from_srl(self.srl_path)
-        truth_cat_df = load_truth_df(self.truth_path)
-        xmatch_df = get_match_df(sub_cat_df, truth_cat_df, self.freq)
-
-        # Append the number of gaussians to the source list dataframe, and set the 
-        # [Source_id] and [id] columns as the source list and truth catalogue 
-        # dataframe indexes respectively.
-        #
-        srl_df = srl_gaul_df(gaul_path, srl_path)
-
-        srl_df_full = srl_df.copy()
-        
-        srl_df = srl_df.set_index("Source_id")
-        match_df = match_df.set_index("id")
-
-        # Set the true size ID for the source list & drop resulting NaN values 
-        # from unmatched sources.
-        #
-        srl_df[size_col] = match_df[size_col]
-        srl_df = self._preprocess_srl_df(srl_df)
-
-        srl_df_full = self._preprocess_srl_df(srl_df_full)
- 
-        train_x = srl_df[SRL_CAT_COLS + SRL_NUM_COLS]
-        train_y = srl_df[size_col].values
-        self._fit(train_x, train_y)
-
-        test_x = srl_df_full[SRL_CAT_COLS + SRL_NUM_COLS]
-        test_y = self._predict(test_x)
-
-        return test_y
-    
-    
-def SKLRandomForestRegression(SKLRegression):
-    def __init__(self, srl_path, truth_cat_path, gaul_path, freq=1400):
-        """
         Args:
             srl_path (`str`): Path to source list (.srl file).
             truth_cat_path (`str`): Path to truth catalogue.
-            gaul_path (`str`): Path to gaussian list (.srl file).
+            gaul_path (`str`): Path to Gaussian list (.srl file).
+            regressand_col: (`str`): Regressand column name.
             freq: (`int`): Frequency band (MHz).
-            algorithm: (`class`) sklearn algorithm class
+            srl_cat_cols: (`list`) Categorical columns in source list.
+            srl_num_cols: (`list`) Numerical columns in source list.
+            srl_drop_cols: (`list`) Columns to exclude in source list.
+        Returns:
+            None
         """
-        super().__init__(srl_path, truth_cat_path, gaul_path, freq)
-        self.regressor = RandomForestRegressor()
+        # Get crossmatched DataFrame using the SDC1 scorer.
+        #
+        xmatch_df = xmatch_using_scorer(srl_path, truth_cat_path, freq)
+
+        # Append the number of Gaussians to the source list DataFrame.
+        #
+        srl_df = srl_gaul_df(gaul_path, srl_path)
+
+        # Reindex both source list and matched dataframes and add matched regressand 
+        # column values to source list DataFrame.
+        #
+        # This leaves NaN values for unmatched sources in <srl_df>.
+        #
+        srl_df = srl_df.set_index("Source_id")
+        xmatch_df = xmatch_df.set_index("id")
+        srl_df[regressand_col] = xmatch_df[regressand_col]
+
+        # Remove unmatched sources and obsolete columns, encode categorical data,
+        # perform any necessary type casting and fit the training set.
+        # 
+        srl_df = self._preprocess_srl_df(srl_df, srl_cat_cols, srl_num_cols, 
+            srl_drop_cols)
+        train_x = srl_df[srl_cat_cols+srl_num_cols]
+        train_y = srl_df[regressand_col].values
+
+        self._SKLfit(train_x, train_y)
 
 
+    def predict(self, srl_path, gaul_path, srl_cat_cols=SRL_CAT_COLS, 
+    srl_num_cols=SRL_NUM_COLS, srl_drop_cols=SRL_COLS_TO_DROP):
+        """
+        Predict the <regressand_column> for a source list using the regressor.
+
+        Args:
+            srl_path (`str`): Path to source list (.srl file).
+            gaul_path (`str`): Path to Gaussian list (.srl file).
+            srl_cat_cols: (`list`) Categorical columns in source list.
+            srl_num_cols: (`list`) Numerical columns in source list.
+            srl_drop_cols: (`list`) Columns to exclude in source list.
+        Returns:
+            (:obj:`numpy.ndarray`): Predicted values.
+        """
+        # Append the number of Gaussians to the source list DataFrame and take a 
+        # copy of the full DataFrame.
+        #
+        srl_df = srl_gaul_df(gaul_path, srl_path)
+
+        # Remove unmatched sources and obsolete columns, encode categorical data,
+        # perform any necessary type casting and fit the full dataset.
+        # 
+        srl_df = self._preprocess_srl_df(srl_df, srl_cat_cols, srl_num_cols, 
+            srl_drop_cols)
+        test_x = srl_df[srl_cat_cols+srl_num_cols]
+        test_y = self._SKLpredict(test_x)
+
+        return test_y
 
 
+    def xmatch_using_scorer(self, srl_path, truth_cat_path):
+        """
+        Crossmatch sources against a truth catalogue using the SDC1 scorer. 
 
+        Args:
+            srl_path (`str`): Path to source list (.srl file).
+            truth_cat_path (`str`): Path to truth catalogue.
+            freq: (`int`): Frequency band (MHz).
+        """
+        sub_cat_df = cat_df_from_srl(srl_path)
+        truth_cat_df = load_truth_df(truth_cat_path)
 
+        scorer = Sdc1Scorer(sub_cat_df, truth_cat_df, freq)
+        score = scorer.run(train=True, detail=True, mode=1)
 
-
-
-def train_classifier(freq, source_df, col_name, algorithm=RandomForestRegressor):
-    '''
-    Given a source dataframe, build a classifier which can predict 'col_name'
-    in the truth catalogue (e.g. 'class', 'b_maj' or 'b_min')
-
-    For development, will require a separate method which performs diagnostic
-    '''
-
-
-    # (See predict_size.py for example workflow)
-    # Get match catalog (by running scorer, or approximately by
-    # astropy.match_catalog_to_sky)
-
-    # Use the Source_id / id columns as the DataFrame index for cross-mapping
-
-    # Get train_x, train_y
-    train_x = np.array([])
-    train_y = np.array([])
-
-    # Train algorithm
-    predictor = algorithm(random_state=0)
-    predictor.fit(train_x, train_y)
-
-    return predictor
+        return score.match_df
