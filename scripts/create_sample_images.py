4f7de2550408ebca612d63854b0e94b5b8ca559a
James Collinson <j.collinson@skatelescope.org>
2020-11-10 23:58:26 +0000

    Fix bug; create sample_image dir if doesn't exist

--- scripts/create_sample_images.py
+++ scripts/create_sample_images.py
@@ -1,16 +1,22 @@
+import os
+from pathlib import Path
+
 from ska.sdc1.utils.image_utils import crop_to_training_area
 
 # Challenge frequency bands
 #
 FREQS = [560, 1400, 9200]
 
+full_image_dir = os.path.join("data", "images")
+sample_image_dir = os.path.join("data", "sample_images")
+
 
 def full_image_path(freq):
-    return "data/images/{}mhz_1000h.fits".format(freq)
+    return os.path.join(full_image_dir, "{}mhz_1000h.fits".format(freq))
 
 
 def sample_image_path(freq):
-    return "data/sample_images/{}mhz_1000h_sample.fits".format(freq)
+    return os.path.join(sample_image_dir, "{}mhz_1000h_sample.fits".format(freq))
 
 
 if __name__ == "__main__":
@@ -22,6 +28,7 @@ if __name__ == "__main__":
 
     for freq in FREQS:
         try:
+            Path(sample_image_dir).mkdir(parents=True, exist_ok=True)
             crop_to_training_area(
                 full_image_path(freq), sample_image_path(freq), freq, 1.5
             )
